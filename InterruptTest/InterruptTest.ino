806248696be93c084e15a0db383a2e418f98be4c
Patrick Gardner <pgardner.753@gmail.com>
2017-12-25 15:16:14 -0600

    Added hueSweep pattern

--- InterruptTest/InterruptTest.ino
+++ InterruptTest/InterruptTest.ino
@@ -1,22 +1,33 @@
-#include <avr/interrupt.h>
 volatile bool val = true;
+#define FASTLED_ALLOW_INTERRUPTS 1
+#include <FastLED.h>
+
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+
+#define NUM_LEDS 16
+#define DATA_PIN 0
+CRGB leds[NUM_LEDS];
+volatile bool val = false;
 
 void setup() {
   delay(2000);
-  pinMode(1, OUTPUT);
-  GIMSK = 0b00100000;    // turns on pin change interrupts
-  PCMSK = 0b00001000;    // turn on interrupts on pins PB3
-  sei();                 // enables interrupts
-
+  pinMode(1, INPUT_PULLUP);
+  sbi(GIMSK,PCIE); // Turn on Pin Change interrupt
+  sbi(PCMSK,PCINT1); // Which pins are affected by the interrupt
+  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.setBrightness(40);
 }
 
 void loop() {
-  digitalWrite(1, val);
+  if(val) {
+    leds[3] = CRGB::Purple;
+    FastLED.show();
+  }
   
 }
 
 
 ISR(PCINT0_vect) {
-  val = !val;
+  val = true;
 }
 
